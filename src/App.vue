dd984f56f3e04eddcbec0bb1aa5bddaa5099d259
camellia785 <choiyuna759@gmail.com>
2025-04-08 15:11:28 +0900

    Feat: 소비카테고리 내역 추가

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
+<script setup></script>
+
 <template>
   <router-view></router-view>
-  <CategorySummary title="2025년 4월 식비 금액" :amount="262200" date="23회" />
 </template>
 
-<script>
-import CategorySummary from './components/CategorySummary.vue'
-
-export default {
-  components: {
-    CategorySummary,
-  },
-}
-</script>
+<style scoped></style>
