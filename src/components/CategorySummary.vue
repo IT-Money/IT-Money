940810a666cdda43d4eae18acf9e5605bd43ba6f
camellia785 <choiyuna759@gmail.com>
2025-04-08 13:07:05 +0900

    Feat: 카테고리 요약 추가

---
+++ src/components/CategorySummary.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div
+    class="bg-purple-100 p-4 rounded-xl flex items-center justify-between mb-4"
+  >
+    <div>
+      <p class="text-sm text-gray-500">{{ month }} {{ category }} 금액</p>
+      <p class="text-xl font-bold">{{ totalAmount.toLocaleString() }}원</p>
+    </div>
+    <img src="/icons/food.png" class="w-10 h-10" alt="카테고리 아이콘" />
+  </div>
+</template>
+
+<style scoped></style>
