95f2adadd5ebcbb58083241f4397185cbcb5fee9
ockko <ocko119@gmail.com>
2025-04-08 17:45:53 +0900

    Feat: Header 구현

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup></script>
+<script setup>
+import HeaderLayout from './components/HeaderLayout.vue'
+</script>
 
 <template>
+  <HeaderLayout />
   <router-view></router-view>
 </template>
 
