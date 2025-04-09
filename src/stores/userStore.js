6d5efe233e9d412d26b892d902257491fb3c858c
sojsnake <sojsnake@naver.com>
2025-04-09 16:19:50 +0900

    Feat: 상태 업데이트 기능

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,12 +1,14 @@
 import { defineStore } from 'pinia'
 import { useUser } from '@/api/useUser'
+import { updateUserNameOnServer } from '@/api/useUser'
 
 export const useUserStore = defineStore('userStore', () => {
   // 상태
   const { userName, userEmail, userPassword, fetchUser } = useUser()
 
-  const updateName = newName => {
-    userName.value = newName
+  const updateName = async newName => {
+    const response = await updateUserNameOnServer(newName)
+    userName.value = response.data.name // 상태 업데이트도 같이
   }
 
   const updateEmail = newEmail => {
