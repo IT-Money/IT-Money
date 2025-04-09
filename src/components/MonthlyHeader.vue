4a51215a56745738954f9d3d06317a2fad6c6bb8
jaehyeonngit <jhjhh023@gmail.com>
2025-04-09 15:16:34 +0900

    Feat: montlyExpense(월별 지출), monthlyIncome(월별 수입)

--- src/components/MonthlyHeader.vue
+++ src/components/MonthlyHeader.vue
@@ -1,30 +1,4 @@
-<script setup>
-import { ref, computed } from 'vue'
-import { onMounted } from 'vue'
-import axios from '@/utils/axios'
-const transactions = ref([])
-const monthlyTransactions = ref([])
-const error = ref(null)
-const monthlyExpense = async () => {
-  try {
-    const response = await axios.get('/transactions')
-    transactions.value = response.data
-    monthlyTransactions.value = transactions.value.filter(trans =>
-      trans.dateTime.startsWith('2025-04'),
-    )
-    console.log(transactions)
-  } catch (err) {
-    error.value = err.message || '거래시간 출력 중 오류 '
-    console.error('오류 발생:', error)
-  }
-}
-const totalAmount = computed(() =>
-  monthlyTransactions.value.reduce((sum, item) => (sum += item.amount), 0),
-)
-onMounted(() => {
-  monthlyExpense()
-})
-</script>
+<script setup></script>
 
 <template>
   <i class="fa-solid fa-angle-left"></i><span>{{}}월 </span>
--- src/views/TransactionHistory.vue
+++ src/views/TransactionHistory.vue
@@ -1,46 +1,23 @@
 <script setup>
-// import CategoryBar from '@/components/CategoryBar.vue'
-// import CategoryList from '@/components/CategoryList.vue'
-import {axios} from '@/utils/axios'
-import MonthlyHeader from '@/components/MonthlyHeader.vue'
-import { ref, computed } from 'vue'
+import MonthlyHeader from '@/components/MonthlyHeader'
+import MonthlyChart from '@/components/MonthlyChart'
+import MonthlyCategory from '@/compoenets/MonthlyCategory'
+import { onMounted } from 'vue'
+import { useTrans } from '@/mine/useTrans'
+const counter = useTrans()
+const currentMonth = new Date().toISOString().substring(0, 7)
 
-//  transaction:  wallet_db에서 transaction 변수 설정
-const transactions = ref([])
-
-// monthlyTransaction: 전체 transaction에서 월별로 담을 변수 설정
-const monthlyTransactions = ref([])
-
-// monthlyExpenses: monthlyTransactions에서 지출들
-const monthlyExpenses = computed((tra
-
-)
-const fetchTransactions = async () => {
- try { const response = await axios.get('/transactions')
-  transactions.value = response.data
-  console.log(transactions);
-} catch (err) {
-  error.message = err.value || '데이터 가져오는 중 오류발생'
-  console.error('거래 fetch 중 오류발생: ', err );
-}
-}
-
-// currentMonth: 월 변수로 설정
-const currentMonth = ref('2025-04')
-
-// filterMonth: transaction 월별로 filter하는 함수
-const filterMonth = computed(() => {
-  transactions.value.filter(trans =>
-    trans.dateTime.startsWith(currentMonth.value),
-  )
+onMounted(() => {
+  counter.fetchTransactions()
+  console.log('마운트됨')
 })
 </script>
 
 <template>
   <MonthlyHeader />
-
-  <CategoryBar />
-  <CategoryList />
+  <MonthlyChart />
+  <MonthlyCategory />
+  {{ currentMonth }}
 </template>
 
 <style scoped></style>
