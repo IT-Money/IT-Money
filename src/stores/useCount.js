9e316d889447c3d9848c8368596214a71bfe90e9
jaehyeonngit <jhjhh023@gmail.com>
2025-04-11 03:39:55 +0900

    Docs: 기존의 useCount, useTrans 전역으로 변경경

--- src/stores/useCount.js
+++
@@ -1,8 +0,0 @@
-export default function useCount() {
-  function useMonthlyAmount(arr = []) {
-    const total = arr.reduce((s, v) => Number((s += v.amount)), 0)
-    return total
-  }
-
-  return { useMonthlyAmount }
-}
